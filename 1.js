ff1fec0ec923df475b61b80d6279e26b21ab483d
AyyanFt <143315031+AyyanFt@users.noreply.github.com>
2023-09-01 12:12:26 +0500

    1.js

--- 1.js
+++ 1.js
@@ -1,23 +1,43 @@
-const   TheNav= document.querySelector('.NavMenu'),    //The Nav 
-        Menu  = document.querySelector('.menu');      // The Menu Btn for Nav
 
+//SideBar Menu
 
-TheNav.style.display = "none";
+const   TheNav= document.querySelector('.sideBar');    //The Nav 
+
+        // Maing Display hidden to make visible later on:
+TheNav.style.width = "72px";
 function ViewNAV() {
     // Function called when you click on 'Menu' DIV element
-    if(TheNav.style.display === "none"){
-        TheNav.style.display = "";
-    } else {
-        TheNav.style.display = "none";
+    if(TheNav.style.width === "72px"){
+        TheNav.style.width = "140px";
+    } else{
+        TheNav.style.width = "72px";
     }
 }
 
 
 
+
+// Cancel btn
+
+const closebtns = document.querySelectorAll('.CancelBtn');
+var i;
+for (i = 0; i < closebtns.length; i++) {
+    closebtns[i].addEventListener("click", function() {
+      this.parentElement.style.display = 'none';
+    });
+  }
+
+
+
+
+
+
+// Profile Menu
+
 const   ProfBtn = document.querySelector('.ProfileMenuBtn'), // The BTN to view User's Profile menu
         ProfMenu= document.querySelector('.UsersMenu');     //  User's Profile menu
 
-        
+        // Maing Display hidden to make visible later on:
 ProfMenu.style.display = "none";
 function ViewMenu() {  
     // Function called when you click on 'Profile Picture' button element
@@ -28,4 +48,36 @@ function ViewMenu() {
         ProfMenu.style.display = "none";
     }
     
+}
+
+//Prof View
+
+const Profoption = document.querySelector('#Prof-option'),
+      ProfView = document.querySelector('.Prof-Div');
+
+        // Making Display none to make it visible later on
+    ProfView.style.display = "none";
+
+Profoption.onclick = () => {
+    if(ProfView.style.display === "none"){
+        ProfView.style.display = "";
+        ProfMenu.style.display = "none";
+    }
+}
+
+// Upload Option
+
+const   SelectorELEM = document.querySelector('.SelType'), // drop-down menu of types
+        Upl_Div = document.querySelector('.Upload-Part'),  // whole Upload Div
+        Uploption = document.querySelector('#Upl-option');  // Profmenu > li = "Upload a Blog"
+
+        // Maing Display hidden to make visible later on:
+    Upl_Div.style.display = "none";
+
+Uploption.onclick = () => {
+    // Function called when you click on Option 'Upload a blog' form Profile Menu  button element
+    if(Upl_Div.style.display === "none"){
+        Upl_Div.style.display = "";
+        ProfMenu.style.display = "none";
+    }
 }
